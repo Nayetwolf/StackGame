0b2dfe2d8da3251477701a57772bb822dfe6bc57
Ilyas Filali <101524577+Nayetwolf@users.noreply.github.com>
2022-06-06 23:29:04 +0100

    Initial commit
    .gitignore
    .gitattributes

---
+++ AndroidStackGame/Assets/Scripts/ScoreText.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    private int score;
+    private TextMeshProUGUI text;
+    
+    private void Start()
+    {
+        text = GetComponent<TMPro.TextMeshProUGUI>();
+        GameManager.OnCubeSpawned += GameManager_OnCubeSpawned;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnCubeSpawned -= GameManager_OnCubeSpawned;
+    }
+
+    private void GameManager_OnCubeSpawned()
+    {
+        score++;
+        text.text = "Score: " + score;
+    }
+}
